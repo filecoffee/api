5fcb7b803ea4b7dc9083040d4d539898365cd30d
lukas <s1120228@student.hsleiden.nl>
2020-06-07 23:00:29 +0200

    Added python example code

---
+++ examples/python/upload.py
@@ -0,0 +1,15 @@
+import requests
+import magic
+
+
+# Before running install it with pip install -r requirements.txt (requirements.txt should be in the same directory)
+filename = "path/to/file.png"
+
+# For automatic mime type detection
+mime = magic.Magic(mime=True)
+mimetype = mime.from_file(filename)
+
+# Uploading the file.
+files = {'file': (filename, open(filename,'rb'), mimetype)}
+r = requests.post(url='https://file.coffee/api/v1/upload?api=your_key', files=files)
+print(r.json())
